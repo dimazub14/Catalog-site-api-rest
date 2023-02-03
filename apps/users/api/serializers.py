6daabeb55f98d5e67212398555046935bd4bb96b
dimazub <dimazub14@gmail.com>
2023-02-03 13:09:44 +0200

    RESET-PASSWORD

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -1,7 +1,22 @@
-from django.contrib.auth.password_validation import validate_password
-from rest_framework import serializers
+from typing import Dict, Any
 
-from apps.users.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+from apps import utils
+from apps.users.services import UserService
+
+User = get_user_model()
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import exceptions, serializers
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions as django_exceptions
+from django.db import IntegrityError, transaction
+from rest_framework import exceptions, serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.settings import api_settings
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -23,3 +38,59 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         """Validate password"""
         validate_password(password=value)
         return value
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    """Reset password serializer"""
+
+    email = serializers.EmailField(required=True)
+
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
+        attrs = super().validate(attrs)
+        attrs = {
+            "email": "someemail"
+        }
+        if not UserService.exists(attrs):
+            raise serializers.ValidationError({"email": "User with email: {} doesn't exists".format(attrs["email"])})
+        return attrs
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(style={"input_type": "password"})
+
+    default_error_messages = {
+        "invalid_token": _("Invalid token for given user."),
+        "invalid_uid": _("Invalid user id or user doesn't exist."),
+    }
+
+    def validate_new_password(self, value):
+        user = getattr(self, "user", None) or self.context["request"].user
+
+        try:
+            validate_password(value, user)
+        except django_exceptions.ValidationError as e:
+            raise serializers.ValidationError({"new_password": list(e.messages)})
+        return value
+    def validate(self, attrs):
+        validated_data = super().validate(attrs)
+        try:
+            uid = utils.decode_uid(self.initial_data.get("uid", ""))
+            self.user = UserService.get({"pk": uid})
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            key_error = "invalid_uid"
+            raise ValidationError(
+                {"uid": [self.error_messages[key_error]]}, code=key_error
+            )
+
+        is_token_valid = default_token_generator.check_token(self.user, self.initial_data.get("token", ""))
+        if is_token_valid:
+            validated_data.update({"user": self.user})
+            return validated_data
+        else:
+            key_error = "invalid_token"
+            raise ValidationError(
+                {"token": [self.error_messages[key_error]]}, code=key_error
+            )
+
--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,12 +1,18 @@
 from django.utils.decorators import method_decorator
 from rest_framework import permissions, status
+from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from apps.users.api.docs import LoginSwagger, RefreshSwagger
-from apps.users.api.serializers import UserRegistrationSerializer
+from apps.users.api.serializers import UserRegistrationSerializer, ResetPasswordSerializer, \
+    PasswordResetConfirmSerializer
+from apps.users.emails import PasswordResetEmail
 from apps.users.services import UserService
+from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+
+User = get_user_model()
 
 
 class RegistrationUserAPIView(GenericAPIView):
@@ -23,9 +29,62 @@ class RegistrationUserAPIView(GenericAPIView):
         self.service_class.create(serializer.validated_data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 @method_decorator(LoginSwagger.extend_schema, name="post")
 class LoginAPIView(TokenObtainPairView):
     """Login APi View"""
+
+
 @method_decorator(RefreshSwagger.extend_schema, name="post")
 class RefreshAPIView(TokenRefreshView):
     """Refresh APi View"""
+
+
+class ResetPasswordAPIView(GenericAPIView):
+    serializer_class = ResetPasswordSerializer
+    service_class = UserService
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.service_class.get(serializer.validated_data)
+
+        if user:
+            context = {"user": user}
+            to = [user.email]
+            PasswordResetEmail(self.request, context).send(to)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # @action(["post"], detail=False)
+    # def reset_password_confirm(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #
+    #     serializer.user.set_password(serializer.data["new_password"])
+    #     if hasattr(serializer.user, "last_login"):
+    #         serializer.user.last_login = now()
+    #     serializer.user.save()
+    #
+    #     if settings.PASSWORD_CHANGED_EMAIL_CONFIRMATION:
+    #         context = {"user": serializer.user}
+    #         to = [get_user_email(serializer.user)]
+    #         settings.EMAIL.password_changed_confirmation(self.request, context).send(to)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ResetPasswordConfirmAPIView(GenericAPIView):
+    serializer_class = PasswordResetConfirmSerializer
+    service_class = UserService
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.service_class.change_password(
+            user=serializer.validated_data["user"],
+            password=serializer.validated_data["new_password"]
+        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
